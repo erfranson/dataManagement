b7c6248276589870c997fa197263a07f36bfd4ec
Ethan Zander <ethanzander13@gmail.com>
2017-07-29 10:00:43 -0600

    JavaScript with Firebase

---
+++ dataManagement.js
@@ -0,0 +1,16 @@
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8Y8ahpCl6Ifi_zhWVRlnDLTR6s7kv9oQ",
+    authDomain: "empdatamanagement.firebaseapp.com",
+    databaseURL: "https://empdatamanagement.firebaseio.com",
+    projectId: "empdatamanagement",
+    storageBucket: "",
+    messagingSenderId: "178185002566"
+  };
+  firebase.initializeApp(config);
+
+
+var dataBase = firebase.dataBase();
+
+
